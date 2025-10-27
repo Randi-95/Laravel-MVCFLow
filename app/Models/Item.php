b425cfd86035915237c717fb5275e81adb9fd207
Randi-95 <randiganteng9528@gmail.com>
2025-10-27 11:18:40 +0700

    DB Seeder and Faker

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    use HasFactory;
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
