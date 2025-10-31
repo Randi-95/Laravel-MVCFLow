db862165efe08e1adb31c532e02fc1090266adfe
Randi-95 <randiganteng9528@gmail.com>
2025-10-31 11:21:41 +0700

    Eloquent ORM View Data

---
+++ resources/views/category.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Category</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>name</th>
+        </tr>
+        @foreach ($categories as $category)
+            <tr>
+                <td>{{ $category->id }}</td>
+                <td>{{ $category->name }}</td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource("category", CategoryController::class);
+Route::resource("item", ItemController::class);
+Route::resource("transaction", TransactionController::class);
